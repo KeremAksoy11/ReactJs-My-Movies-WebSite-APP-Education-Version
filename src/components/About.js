e765fdad5ae0a5b8fe574c008f593c3baf0f8852
MEHMET KEREM AKSOY <mkeremaksoy@stu.aydin.edu.tr>
2022-02-03 19:03:16 +0300

    Deneme

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+class About extends React.Component{
+    render(){
+        return(
+            <div>
+                    <h6 className="text-left">All Rights Reserved.</h6>
+            </div>
+   
+  
+        )
+    }
+}
+
+export default About;
